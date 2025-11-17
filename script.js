3cab49fc5fea11044d2f422cb112f092b4eb21f6
jaquelinegoulart-creator <jaqueline.goulart@escola.pr.gov.br>
2025-11-17 19:23:03 -0300

    lo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let nome = document.querySelector("#texto");
 nome.textContent = "Jaqueline";
 
-let nome = prompt ("Qual o seu nome?");
+let nome = prompt ("Qual é o seu nome?");
 texto.textContent = nome;
